ebb3eb3aa54766708479bdec26ca293b958fb1be
Raheel Jameel <raheel_jameel@yahoo.com>
2019-05-05 01:09:23 +0500

    Adding Custom Typings for Koa Context State

---
+++ src/custom-typings/koa/index.d.ts
@@ -0,0 +1,35 @@
+import { Context } from 'koa';
+
+declare module 'koa' {
+  interface Context {
+    state: {
+
+      /**
+       * Data to be sent back in the response data
+       *
+       * @type {*}
+       */
+      data?: any;
+
+      /**
+       * Message to be sent back in the response meta message
+       *
+       * @type {string}
+       */
+      message?: string;
+
+      /**
+       * Set the response status code
+       *
+       * If not set and Context.data field is set then status code of 200 will be returned
+       *
+       * If not set and Context.data field is also not set then status code of 404 will be returned
+       *
+       * @type {number}
+       */
+      status?: number;
+
+      [index: string]: any
+    }
+  }
+}
